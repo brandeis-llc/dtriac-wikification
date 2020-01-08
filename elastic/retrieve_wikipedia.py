d8fc77bf72cae043bd058f3a60dbd059d1a902c4
jxtu <jxtu@brandeis.edu>
2020-01-08 10:25:37 -0500

    adding scripts retrieving docs from ES index

--- elastic/retrieve_wikipedia.py
+++ elastic/retrieve_wikipedia.py
@@ -2,10 +2,30 @@
 Provides helper functions for retrieving data from ES indices of wikipedia articles.
 """
 
+from os.path import join as pjoin
+from elasticsearch import Elasticsearch, helpers
 
-def get_all_text(es_index_name):
-    pass
+
+def export_es_data(es, out_dir, index_name, field):
+    """
+    export documents from ES index
+    :param es: Elasticsearch object
+    :param out_dir: folder to hold all the documents
+    :param index_name: ES index name
+    :param field: specify the field you want to export
+    :return: a fold of txt files with the doc id as names ("[doc_id].txt")
+    """
+    index = helpers.scan(es, query={"query": {"match_all": {}}}, index=index_name)
+    for i, doc in enumerate(index, 1):
+        with open(pjoin(out_dir, f"{doc['_id']}.txt"), 'w') as f:
+            abstract = doc['_source'].get(field)
+            if abstract:
+                f.write(abstract)
+        if i % 1000 == 0:
+            print(f"working on {i} documents!")
 
 
-def get_all_opening_text(es_index_name):
-    pass
+if __name__ == "__main__":
+    es = Elasticsearch("http://tarski.cs-i.brandeis.edu:9200/")
+    export_es_data(es, out_dir='wiki_data', index_name='enwiki-nuke_tech', field='opening_text')
+    export_es_data(es, out_dir='dtra_data', index_name='dtriac-19d', field='text')
