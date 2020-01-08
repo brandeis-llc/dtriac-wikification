f361316bcb51f0bb6c18ea57c1c7e48079f0d803
Keigh Rim <keigh.rim@gmail.com>
2020-01-08 15:35:14 -0500

    added CLI to retrieval script

--- elastic/retrieve_wikipedia.py
+++ elastic/retrieve_wikipedia.py
@@ -4,9 +4,10 @@ Provides helper functions for retrieving data from ES indices of wikipedia artic
 
 from os.path import join as pjoin
 from elasticsearch import Elasticsearch, helpers
+from config import ES_HOST
 
 
-def export_es_data(es, out_dir, index_name, field):
+def export_es_data(es, out_dir, index_name, field, verbose=1):
     """
     export documents from ES index
     :param es: Elasticsearch object
@@ -18,14 +19,42 @@ def export_es_data(es, out_dir, index_name, field):
     index = helpers.scan(es, query={"query": {"match_all": {}}}, index=index_name)
     for i, doc in enumerate(index, 1):
         with open(pjoin(out_dir, f"{doc['_id']}.txt"), 'w') as f:
-            abstract = doc['_source'].get(field)
-            if abstract:
-                f.write(abstract)
-        if i % 1000 == 0:
-            print(f"working on {i} documents!")
+            value = doc['_source'].get(field)
+            if value:
+                f.write(value)
+        if verbose > 0:
+            if i % 1000 == 0:
+                print(f"working on {i} documents!")
 
 
 if __name__ == "__main__":
-    es = Elasticsearch("http://tarski.cs-i.brandeis.edu:9200/")
-    export_es_data(es, out_dir='wiki_data', index_name='enwiki-nuke_tech', field='opening_text')
-    export_es_data(es, out_dir='dtra_data', index_name='dtriac-19d', field='text')
+    import argparse
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=__doc__
+    )
+    parser.add_argument(
+        '-i', '--index',
+        default='',
+        action='store',
+        nargs='?',
+        help='ES index name.'
+    )
+    parser.add_argument(
+        '-f', '--field',
+        default='',
+        action='store',
+        nargs='?',
+        help='Field name to retrieve.'
+    )
+    parser.add_argument(
+        '-o', '--outname',
+        default='',
+        action='store',
+        nargs='?',
+        help='Directory name to put output. Retrieved field value will be saved with <_id>.txt in this directory. '
+             '<_id> is the index number of the document in the ES index. '
+    )
+    args = parser.parse_args()
+    es = Elasticsearch(ES_HOST)
+    export_es_data(es, out_dir=args.outname, index_name=args.index, field=args.field)
