d544e4cf972d3c866d78ce8d495f7632dc2a3302
Keigh Rim <keigh.rim@gmail.com>
2020-01-09 17:33:32 -0500

    added ES-based wikification code

---
+++ wikify/__init__.py
@@ -0,0 +1,6 @@
+class Wikifier(object):
+    # max size of list of wikipedia titles
+    wikification_size = 10
+
+    def wikify(self, text):
+        raise NotImplementedError
---
+++ wikify/by_es.py
@@ -0,0 +1,18 @@
+import elastic
+from elasticsearch_dsl import Search
+from . import Wikifier
+
+
+class WikifyByES(Wikifier):
+    def __init__(self, es_index_name):
+        self.index_name = es_index_name
+        self.es = elastic.es
+
+    def wikify(self, text):
+        q = Search(using=self.es, index=self.index_name).query("match", text=text)
+        hits = q.execute()
+        try:
+            return [hit.title for hit in hits[0:min(self.wikification_size, len(hits))]]
+        except IndexError:
+            return None
+
